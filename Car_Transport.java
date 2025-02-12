f8f9ca9fb50395747a3885b37067c1555933c432
JustJunior5 <150825043+JustJunior5@users.noreply.github.com>
2025-02-12 13:42:52 +0100

    Car_Transport.java finish

--- Car_Transport.java
+++ Car_Transport.java
@@ -31,7 +31,7 @@ public class Car_Transport extends Car{
     public void move() {
         super.move();
         for(Car c: loadedCars){
-            c.move();
+            c.setPosition(getX(),getY());
         }
     }
     public void turnLeft(int angle) {
@@ -51,7 +51,7 @@ public class Car_Transport extends Car{
         Car unloadedCar = loadedCars.getLast();
         if(getCurrentSpeed() == 0 && !rampIsUp){
             loadedCars.remove(unloadedCar);
-            //unloadedCar.x
+            unloadedCar.setPosition(getX() + 2*Math.cos(getTurningAngle()), getY() + 2*Math.sin(getTurningAngle()));
         }
     }
 }
